3212f6686e5cdacda8e193aa56c39759fa43acd4
markeastman <mark@eastmans.me.uk>
2021-07-01 16:17:34 +0100

    Update main.py
    Changed code for new version of the BME280 class

--- src/main.py
+++ src/main.py
@@ -5,6 +5,6 @@ from BME280 import BME280
 
 i2c = I2C(scl=Pin(4), sda=Pin(0), freq=10000)
 bme280 = BME280(i2c=i2c)
-bme280.values
-
-print( "Values were printed")
+print( "Temperature ", bme280.temperature )
+print( "Humidity ", bme280.humidity )
+print( "Pressure ", bme280.pressure )
